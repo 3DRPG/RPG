77949bbdeb1aec6725c679f0f05f60af3c300a32
Bishal Heuju <b.heuju@yahoo.com>
2016-11-11 09:38:47 +0545

    3rd persion character controller with mouse lookup using unity-chan and standard assets

---
+++ Assets/Scripts/respawn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class respawn : MonoBehaviour {
+	
+	private Vector3 startPos;
+	private Quaternion startRot;
+	
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		startRot = transform.rotation;
+	}
+
+	//Detect collision with collider DeathZone
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "DeathZone") {
+			transform.position = startPos;
+			transform.rotation = startRot;
+
+			GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+			GetComponent<Rigidbody>().angularVelocity = new Vector3(0,0,0);
+
+
+		} else if (col.tag == "checkpoint"){
+			startPos = col.transform.position;
+			startRot = col.transform.rotation;
+		}
+	}
+
+}
